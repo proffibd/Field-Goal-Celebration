80d56bd0d18672ca0b62bf879c23dba1c0abcbad
proffibd <123525432+proffibd@users.noreply.github.com>
2023-05-12 14:20:58 -0400

    Delete Group_project directory

--- Group_project/libTouch/touch_settings.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOUCH_SETTINGS_H_INCLUDED
-#define TOUCH_SETTINGS_H_INCLUDED
-
-//----- Configuration --------------------------//
-//I2C pins -				PORT, PIN
-#define y_pin		C, 2 // Arduino Analog 2
-#define x_pin		C, 1 // Arduino Analog 1
-//----------------------------------------------//
-#endif
